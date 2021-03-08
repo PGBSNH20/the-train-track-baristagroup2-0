75fb85e2c7c56049f9750f3e43a76d39dc6ecb73
joakimwidell <joakimwidell@hotmail.com>
2021-03-08 15:19:40 +0100

    Simple commit

---
+++ Source/TrainConsole/TrainPlanner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainConsole
+{
+    public class TrainPlanner : IPlannerSchedule
+    {
+        public TrainPlanner(Train train)
+        {
+            this.train = train;
+        }
+        public Train train { get; set; }
+
+        public TrainPlanner FollowSchedule()
+        {
+            
+        }
+    }
+    public interface IPlannerSchedule 
+    {
+        public TrainPlanner FollowSchedule();
+    }
+}
