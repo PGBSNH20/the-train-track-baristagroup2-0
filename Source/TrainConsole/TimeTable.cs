3925b9bdae129c53148017283bde5969d18eb765
KevinKallin <kevin.kallin@hotmail.se>
2021-03-08 15:59:52 +0100

    Update TimeTable.cs

--- Source/TrainConsole/TimeTable.cs
+++ Source/TrainConsole/TimeTable.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TrainConsole
+namespace TrainConsole 
 {
-    public class TimeTable
+    public class TimeTable : IPlannerSchedule
     {
         public int TrainId { get; set; }
         public int StationId { get; set; }
         public string DepartureTime { get; set; }
         public string ArrivalTime { get; set; }
+
+        public IPlannerSchedule StartTrainAt(string startTime)
+        {
+            Console.WriteLine(startTime);
+            return this;
+        }
+        public IPlannerSchedule StopTrainAt(string stopTime)
+        {
+            Console.WriteLine(stopTime);
+            return this;
+        }
+
+        public TimeTable ToPlan()
+        {
+            return new TimeTable();
+        }
     }
 }
 
