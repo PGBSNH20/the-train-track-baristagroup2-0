ef5387af8d8117a290d72c9c9b5cd117b3f282b8
joakimwidell <joakimwidell@hotmail.com>
2021-03-09 10:44:58 +0100

    Commit as usual

--- Source/TrainConsole/file.cs
+++ Source/TrainConsole/file.cs
@@ -10,15 +10,15 @@ namespace TrainConsole
    public class file
     {
 
-        static string timeTableFilePath = @"TextFiles\timetable.txt";
-        static string trainsFilePath = @"TextFiles\trains.txt";
-        static string stationsFilePath = @"TextFiles\stations.txt";
+        public static string timeTableFilePath = @"TextFiles\timetable.txt";
+        public static string trainsFilePath = @"TextFiles\trains.txt";
+        public static string stationsFilePath = @"TextFiles\stations.txt";
         //public static string passengerFilePath = @"TextFiles\passengers.txt";
 
-        public List<TimeTable> filePathTimeTable(string filePath)
+        public List<TimeTable> filePathTimeTable(string timeTableFilePath)
         {
              var timeTable = new List<TimeTable>();
-             string[] lines = File.ReadAllLines(filePath);
+             string[] lines = File.ReadAllLines(timeTableFilePath);
 
             foreach (var line in lines)
             {
@@ -35,6 +35,45 @@ namespace TrainConsole
             }
             return timeTable;
         }
-     
+        public List<Train> filePathTrains(string trainsFilePath)
+        {
+            var trainList = new List<Train>();
+            string[] lines = File.ReadAllLines(trainsFilePath);
+
+            foreach (var line in lines)
+            {
+                string[] parts = line.Split(',');
+
+                var trainsInList = new Train
+                {
+                    Id = int.Parse(parts[0]),
+                    Name = parts[1],
+                    MaxSpeed = int.Parse(parts[2]),
+                    Operated = bool.Parse(parts[3])
+                };
+                trainList.Add(trainsInList);
+            }
+            return trainList;
+        }
+        public List<Station> filePathStations(string stationsFilePath)
+        {
+            var stationList = new List<Station>();
+            string[] lines = File.ReadAllLines(stationsFilePath);
+
+            foreach (var line in lines)
+            {
+                string[] parts = line.Split(',');
+
+                var stationsInList = new Station
+                {
+                    Id = int.Parse(lines[0]),
+                    StationName = lines[1],
+                    EndStation = bool.Parse(lines[2])
+                };
+                stationList.Add(stationsInList);
+            }
+            return stationList;
+        }
+
     }
 }
