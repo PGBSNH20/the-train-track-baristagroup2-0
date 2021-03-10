ce2ef2d32c85581d402dbbd4ee38b05e66ae5c63
RobinAxelsson <axelsson.robin@outlook.com>
2021-03-10 09:29:56 +0100

    New Level

---
+++ Source/FirstLevelRailway/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstLevelRailway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
