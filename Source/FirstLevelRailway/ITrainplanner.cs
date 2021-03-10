576dd35f01478caf8f6b8c72f2310078839b81c8
SaAlgervik <70169634+SaAlgervik@users.noreply.github.com>
2021-03-10 16:08:32 +0100

    Made interface with fuent API
    Made the start for making new timeTable for trains with help of a fluent API

---
+++ Source/FirstLevelRailway/ITrainplanner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstLevelRailway
+{
+    public interface ITrainplanner
+    {
+        ITrainplanner FollowSchedule(TrainPlanner timeTable);
+        ITrainplanner StartTrainAt(string departureStation, string startTime);
+        ITrainplanner ArriveTrainAt(string arriveAtStation, string arrivalTime);
+        ITrainplanner GeneratePlan();
+    }
+
+    public class TrainPlanner : ITrainplanner
+    {
+        public string DepartureAt;
+        public string StartTime { get; set; }
+        public string ArriveAtStation { get; set; }
+        public string ArrivalTime { get; set; }
+
+        public ITrainplanner StartTrainAt(string departureStation, string startTime)
+        {
+            new TrainPlanner() { DepartureAt = departureStation, StartTime = startTime };
+            return this;
+        }
+        public ITrainplanner ArriveTrainAt(string arriveAtStation, string time)
+        {
+            new TrainPlanner() { ArriveAtStation = arriveAtStation, ArrivalTime = time };
+            return this;
+        }
+
+        public ITrainplanner FollowSchedule(TrainPlanner timeTable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrainplanner GeneratePlan()
+        {
+            Console.WriteLine("You time table have been saved");
+
+            // trainplan.save();
+            return this;
+        }
+    }
+}
