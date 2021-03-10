ab116787e2a2083d6832f109dd29d6fda5afe613
joakimwidell <joakimwidell@hotmail.com>
2021-03-10 11:08:48 +0100

    Update Program.cs

--- Source/FirstLevelRailway/Program.cs
+++ Source/FirstLevelRailway/Program.cs
@@ -14,12 +14,24 @@ namespace FirstLevelRailway
             train1.Move();
             Thread.Sleep(400);
             }
+            Thread trainOne = new Thread(new ThreadStart(train1Work());
+            
 
+        }
+        static void train1Work(List<String> Route)
+        {
+
+            for (int i = 0; i < Route.Count; i++)
+            {
+                Console.WriteLine(Route[i]);
+                Thread.Sleep(400);
+            }
         }
     }
     public class Train
     {
         private int position { get; set; } = 0;
+
         public List<string> Route { get; set; } = new List<string>
         {
             "Avgår från Alingsås",
