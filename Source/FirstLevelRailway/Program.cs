b12c633397edc8776a10cce774ec6e959ab3c0f8
RobinAxelsson <axelsson.robin@outlook.com>
2021-03-11 08:59:18 +0100

    Charmover added increments right

--- Source/FirstLevelRailway/Program.cs
+++ Source/FirstLevelRailway/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Transactions;
 
@@ -9,17 +10,26 @@ namespace FirstLevelRailway
     {
         static void Main(string[] args)
         {
-            var trainData = TrainCallLoader.Load();
-            var trains = Train.GenerateFrom(trainData);
-            var trainThreads = new List<Thread>();
-            foreach (var train in trains)
+            var charMover = new CharMover('z', (5, 5));
+
+            for (int i = 0; i < 10; i++)
             {
-                trainThreads.Add( new Thread(new ThreadStart(() => TrainThread(train))));
+                Thread.Sleep(500);
+                charMover.MoveRight();
             }
-            foreach (var thread in trainThreads)
-            {
-                thread.Start();
-            }
+
+            Console.ReadLine();
+            //var trainData = TrainCallLoader.Load();
+            //var trains = Train.GenerateFrom(trainData);
+            //var trainThreads = new List<Thread>();
+            //foreach (var train in trains)
+            //{
+            //    trainThreads.Add( new Thread(new ThreadStart(() => TrainThread(train))));
+            //}
+            //foreach (var thread in trainThreads)
+            //{
+            //    thread.Start();
+            //}
         }
         static void TrainThread(Train train)
         {
@@ -28,6 +38,30 @@ namespace FirstLevelRailway
                 train.Move();
                 Thread.Sleep(500);
             }
+        }
+    }
+    public class Clock
+    {
+
+    }
+    public class CharMover
+    {
+        char Chr;
+        (int X, int Y) LastCoord;
+        public CharMover(char chr, (int X, int Y) startCoord)
+        {
+            Chr = chr;
+            LastCoord = startCoord;
+            Write(startCoord);
+        }
+        public void MoveRight()
+        {
+            Write((LastCoord.X++, LastCoord.Y));
+        }
+        private void Write((int X, int Y) coord)
+        {
+            Console.SetCursorPosition(coord.X, coord.Y);
+            Console.Write(Chr);
         }
     }
 }
