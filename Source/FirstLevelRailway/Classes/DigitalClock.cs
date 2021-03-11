3f513648a9ae81c2606b0409cbef142e83bfaed0
RobinAxelsson <axelsson.robin@outlook.com>
2021-03-11 13:11:44 +0100

    digital clock testing

---
+++ Source/FirstLevelRailway/Classes/DigitalClock.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace FirstLevelRailway
+{
+    public class DigitalClock
+    {
+        (int x, int y) LeftPosition;
+        int Tick_ns;
+
+        SingleDigit singleMinute;
+        SingleDigit tenMinute;
+        SingleDigit singleHour;
+        SingleDigit tenHour;
+        public DigitalClock((int x, int y) leftPosition, int tick_ns)
+        {
+            LeftPosition = leftPosition;
+            Tick_ns = tick_ns;
+
+            singleMinute = new SingleDigit((LeftPosition.x + 4, LeftPosition.y), 9);
+            tenMinute = new SingleDigit((LeftPosition.x + 3, LeftPosition.y), 5);
+            singleHour = new SingleDigit((LeftPosition.x + 1, LeftPosition.y), 9);
+            tenHour = new SingleDigit(LeftPosition, 2);
+            ConsoleWriter.Write(':', (LeftPosition.x + 2, LeftPosition.y));
+        }
+        public string Tick()
+        {
+            int singleMinuteDigit = 0;
+            int tenMinuteDigit = 0;
+            int singleHourDigit = 0;
+            int tenHourDigit = 0;
+            bool past20 = false;
+
+            singleMinuteDigit = singleMinute.IncrementDigit();
+            if (singleMinuteDigit == 0)
+            {
+                tenMinuteDigit = tenMinute.IncrementDigit();
+                if (tenMinuteDigit == 0)
+                {
+                    singleHourDigit = singleHour.IncrementDigit();
+                    if (singleHourDigit == 0)
+                    {
+                        tenHourDigit = tenHour.IncrementDigit();
+                        if (tenHourDigit == 2)
+                            singleHour.lastDigit = 3;
+                        else
+                            singleHour.lastDigit = 9;
+
+                        past20 = !past20;
+                    }
+                }
+            }
+            return $"{tenHourDigit}{singleHourDigit}:{tenMinuteDigit}{singleMinuteDigit}";
+        }
+        public void StartClock(int maxTicks)
+        {
+            int tick = 0;
+            
+            while (tick < maxTicks)
+            {
+                Thread.Sleep(Tick_ns);
+                Tick();
+                tick++;
+            }
+        }
+        
+    }
+}
---
+++ Source/FirstLevelRailway/Classes/SingelDigit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstLevelRailway
+{
+    public class SingleDigit
+    {
+        int currentDigit = 0;
+        public int lastDigit = 9;
+        public SingleDigit((int x, int y) fixedCoord, int lastDigit)
+        {
+            FixedCoord = fixedCoord;
+            this.lastDigit = lastDigit;
+            WriteDigit(0);
+        }
+        (int x, int y) FixedCoord;
+
+        private void WriteDigit(int digit)
+        {
+            Console.SetCursorPosition(FixedCoord.x, FixedCoord.y);
+            Console.Write(digit);
+        }
+        public int IncrementDigit()
+        {
+            currentDigit++;
+            bool pastLastDigit = currentDigit > lastDigit;
+
+            if (pastLastDigit == true)
+                currentDigit = 0;
+
+            WriteDigit(currentDigit);
+
+            return currentDigit;
+        }
+    }
+}
