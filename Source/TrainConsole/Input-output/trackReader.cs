a9487423ed4b3e8f2c2bd3ca136215df0b5ccf4d
RobinAxelsson <axelsson.robin@outlook.com>
2021-03-11 14:08:53 +0100

    trackchanges

---
+++ Source/TrainConsole/Input-output/trackReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainConsole
+{
+    public class trackReader
+    {
+        public static string FilePath = @"C:\Users\axels\Google Drive\Code\VS Code\Code-Dataatkomster-dotnet\the-train-track-baristagroup2-0\Source\TrainConsole\TextFiles\traintrack.txt";
+        public static List<(char chr, int X, int Y)> CharCoordinates { get; set; }
+        public static List<(char chr, int X, int Y)> Read()
+        {
+            var lines = File.ReadAllLines(FilePath);
+            var charCoord = new List<(char chr, int X, int Y)>();
+
+            int x = 0;
+            int y = 0;
+            foreach (var line in lines)
+            {
+                foreach (char chr in line)
+                {
+                    charCoord.Add((chr, x, y));
+                    x++;
+                }
+                y++;
+                x = 0;
+            }
+            return CharCoordinates = charCoord;
+        }
+    }
+}
---
+++ Source/TrainConsole/Time/DigitalClock.cs
@@ -0,0 +1,65 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Threading;
+
+//namespace FirstLevelRailway
+//{
+//    public class DigitalClock
+//    {
+//        (int x, int y) LeftPosition;
+//        int Tick_ns;
+
+//        SingleDigit singleMinute;
+//        SingleDigit tenMinute;
+//        SingleDigit singleHour;
+//        SingleDigit tenHour;
+//        public DigitalClock((int x, int y) leftPosition, int tick_ns)
+//        {
+//            LeftPosition = leftPosition;
+//            Tick_ns = tick_ns;
+
+//            singleMinute = new SingleDigit((LeftPosition.x + 4, LeftPosition.y), 9);
+//            tenMinute = new SingleDigit((LeftPosition.x + 3, LeftPosition.y), 5);
+//            singleHour = new SingleDigit((LeftPosition.x + 1, LeftPosition.y), 9);
+//            tenHour = new SingleDigit(LeftPosition, 2);
+//            ConsoleWriter.Write(':', (LeftPosition.x + 2, LeftPosition.y));
+//        }
+//        public void StartClock(int maxTicks)
+//        {
+//            int tick = 0;
+//            bool past20 = false;
+//            int singleMinuteDigit;
+//            int tenMinuteDigit;
+//            int singleHourMinuteDigit;
+//            int tenHourDigit;
+
+//            while (tick < maxTicks)
+//            {
+//                Thread.Sleep(Tick_ns);
+
+//                singleHourMinuteDigit = singleMinute.IncrementDigit();
+//                if (singleHourMinuteDigit == 0)
+//                {
+//                    tenMinuteDigit = tenMinute.IncrementDigit();
+//                    if (tenMinuteDigit == 0)
+//                    {
+//                        singleHourMinuteDigit = singleHour.IncrementDigit();
+//                        if (singleHourMinuteDigit == 0)
+//                        {
+//                            tenHourDigit = tenHour.IncrementDigit();
+//                            if (tenHourDigit == 2)
+//                                singleHour.lastDigit = 3;
+//                            else
+//                                singleHour.lastDigit = 9;
+
+//                            past20 = !past20;
+//                        }
+//                    }
+//                }
+//                tick++;
+//            }
+//        }
+        
+//    }
+//}
