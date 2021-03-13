09d313b478815e0727a69840c83caec5c085be93
RobinAxelsson <axelsson.robin@outlook.com>
2021-03-13 07:05:42 +0100

    clock simplifyed

--- Source/TrainConsole/Time/TwentyFourHourClock.cs
+++ Source/TrainConsole/Time/TwentyFourHourClock.cs
@@ -8,71 +8,30 @@ namespace TrainConsole
 {
     public class TwentyFourHourClock
     {
-        public string[] Time { get; set; } = new string[] { "0", "0", ":", "0", "0" };
+        public string Time { get; set; } = "00:00";
         public int Ticks { get; set; } = 0;
-        public string[] TickOnce()
+        public int GetDays()
+            => Ticks / 1440;
+        public void Tick()
         {
-            TickTimes(1);
-            return this.Time;
+            Ticks++;
+            int thisDayTicks = Ticks % 1440;
+            int hours = thisDayTicks / 60;
+            int min = thisDayTicks % 60;
+
+            string h = hours.ToString();
+            if (h.Length == 1)
+                h = "0" + h;
+            string m = min.ToString();
+            if (m.Length == 1)
+                m = "0" + m;
+            Time = h + ":" + m;
         }
         public void TickTimes(int times)
         {
-            string min = "0";
-            int oneMin = 0;
-            int tenMin = 0;
-            int oneHour = 0;
-            int tenHour = 0;
-            var time = Time;
-            var tick24 = 0;
-
             for (int i = 0; i < times; i++)
             {
-                Ticks++;
-                tick24++;
-                oneMin++;
-                time[4] = oneMin.ToString()[^1].ToString();
-
-                if (tick24 % 10 == 0)
-                {
-                    tenMin++;
-                    time[3] = tenMin.ToString();
-                }
-                if (tick24 % 10 == 0)
-                {
-                    oneMin = 0;
-                    time[4] = oneMin.ToString();
-                }
-                if (tick24 % 60 == 0)
-                {
-                    tenMin = 0;
-                    time[3] = tenMin.ToString();
-                }
-
-                if (tick24 % 60 == 0)
-                {
-                    oneHour++;
-                    time[1] = oneHour.ToString();
-                }
-                if (tick24 % 600 == 0)
-                {
-                    oneHour = 0;
-                    time[1] = oneHour.ToString();
-                }
-                if (tick24 % 600 == 0)
-                {
-                    tenHour++;
-                    time[0] = tenHour.ToString();
-                }
-                if(tick24 % 1440 == 0)
-                {
-                    tenHour = 0;
-                    time[0] = tenHour.ToString();
-
-                    oneHour = 0;
-                    time[1] = oneHour.ToString();
-
-                    tick24 = 0;
-                }
+                Tick();
             }
         }
     }
--- Source/TrainEngine.Tests/TwentyFourHourClockTests.cs
+++ Source/TrainEngine.Tests/TwentyFourHourClockTests.cs
@@ -115,41 +115,27 @@ namespace TrainEngine.Tests
         public void Modulus_InsertMinutesDigital_Expect_01_01()
         {
             int ticks = 61;
-            int thisDayTicks = ticks % 1440;
-            int hours = thisDayTicks / 60;
-            int min = thisDayTicks % 60;
-            string h = hours.ToString();
-            string m = min.ToString();
-            string digital = "0" + h + ":0" + m;
+            var clock = new TwentyFourHourClock();
+            clock.TickTimes(ticks);
 
-            Assert.True(digital == "01:01");
+            Assert.True(clock.Time == "01:01");
         }
         [Fact]
         public void Modulus_InsertMinutesDigital_Expect_01_11()
         {
             int ticks = 71;
-            int thisDayTicks = ticks % 1440;
-            int hours = thisDayTicks / 60;
-            int min = thisDayTicks % 60;
-            string h = hours.ToString();
-            string m = min.ToString();
-
-            string digital = "0" + h + ":" + m;
-
+            var clock = new TwentyFourHourClock();
+            clock.TickTimes(ticks);
+            string digital = clock.Time;
             Assert.True(digital == "01:11");
         }
         [Fact]
         public void Modulus_InsertFullDigital_Expect_00_01()
         {
             int ticks = 1;
-            int thisDayTicks = ticks % 1440;
-            int hours = thisDayTicks / 60;
-            int min = thisDayTicks % 60;
-            string h = hours.ToString();
-            string m = min.ToString();
-            if (m.Length == 1)
-                m = "0" + m;
-            string digital = "0" + h + ":" + m;
+            var clock = new TwentyFourHourClock();
+            clock.TickTimes(ticks);
+            string digital = clock.Time;
 
             Assert.True(digital == "00:01");
         }
@@ -157,15 +143,9 @@ namespace TrainEngine.Tests
         public void Modulus_InsertFullDigital_Expect_11_colon_01()
         {
             int ticks = 661;
-            int thisDayTicks = ticks % 1440;
-            int hours = thisDayTicks / 60;
-            int min = thisDayTicks % 60;
-            
-            string h = hours.ToString();
-            string m = min.ToString();
-            if (m.Length == 1)
-                m = "0" + m;
-            string digital = h + ":" + m;
+            var clock = new TwentyFourHourClock();
+            clock.TickTimes(ticks);
+            string digital = clock.Time;
 
             Assert.True(digital == "11:01");
         }
@@ -173,17 +153,9 @@ namespace TrainEngine.Tests
         public void Modulus_InsertFullDigital_Expect_01_colon_01()
         {
             int ticks = 61;
-            int thisDayTicks = ticks % 1440;
-            int hours = thisDayTicks / 60;
-            int min = thisDayTicks % 60;
-
-            string h = hours.ToString();
-            if (h.Length == 1)
-                h = "0" + h;
-            string m = min.ToString();
-            if (m.Length == 1)
-                m = "0" + m;
-            string digital = h + ":" + m;
+            var clock = new TwentyFourHourClock();
+            clock.TickTimes(ticks);
+            string digital = clock.Time;
 
             Assert.True(digital == "01:01");
         }
@@ -191,18 +163,10 @@ namespace TrainEngine.Tests
         public void Modulus_DayFullDigital_Expect_0Days_01_colon_01()
         {
             int ticks = 61;
-            int days = ticks / 1440;
-            int thisDayTicks = ticks % 1440;
-            int hours = thisDayTicks / 60;
-            int min = thisDayTicks % 60;
-
-            string h = hours.ToString();
-            if (h.Length == 1)
-                h = "0" + h;
-            string m = min.ToString();
-            if (m.Length == 1)
-                m = "0" + m;
-            string digital = h + ":" + m;
+            var clock = new TwentyFourHourClock();
+            clock.TickTimes(ticks);
+            string digital = clock.Time;
+            int days = clock.GetDays();
 
             Assert.True(digital == "01:01" && days == 0);
         }
@@ -210,63 +174,32 @@ namespace TrainEngine.Tests
         public void Modulus_DayFullDigital_Expect_1Days_01_colon_01()
         {
             int ticks = 1501;
-            int days = ticks / 1440;
-            int thisDayTicks = ticks % 1440;
-            int hours = thisDayTicks / 60;
-            int min = thisDayTicks % 60;
-
-            string h = hours.ToString();
-            if (h.Length == 1)
-                h = "0" + h;
-            string m = min.ToString();
-            if (m.Length == 1)
-                m = "0" + m;
-            string digital = h + ":" + m;
+            var clock = new TwentyFourHourClock();
+            clock.TickTimes(ticks);
+            string digital = clock.Time;
+            int days = clock.GetDays();
 
             Assert.True(digital == "01:01" && days == 1);
         }
         [Fact]
         public void Modulus_DayFullDigital_Expect_1Days_11_colon_11()
         {
-            //11*60 = 660
-            //24*60=1440
-            //660+1440+11=2111
             int ticks = 2111;
-            int days = ticks / 1440;
-            int thisDayTicks = ticks % 1440;
-            int hours = thisDayTicks / 60;
-            int min = thisDayTicks % 60;
-
-            string h = hours.ToString();
-            if (h.Length == 1)
-                h = "0" + h;
-            string m = min.ToString();
-            if (m.Length == 1)
-                m = "0" + m;
-            string digital = h + ":" + m;
+            var clock = new TwentyFourHourClock();
+            clock.TickTimes(ticks);
+            string digital = clock.Time;
+            int days = clock.GetDays();
 
             Assert.True(digital == "11:11" && days == 1);
         }
         [Fact]
         public void Modulus_DayFullDigital_Expect_1Days_23_colon_01()
-        {
-            //11*60 = 660
-            //24*60=1440
-            //660+1440+11=2111
-            //+720=2831
+        { 
             int ticks = 2831;
-            int days = ticks / 1440;
-            int thisDayTicks = ticks % 1440;
-            int hours = thisDayTicks / 60;
-            int min = thisDayTicks % 60;
-
-            string h = hours.ToString();
-            if (h.Length == 1)
-                h = "0" + h;
-            string m = min.ToString();
-            if (m.Length == 1)
-                m = "0" + m;
-            string digital = h + ":" + m;
+            var clock = new TwentyFourHourClock();
+            clock.TickTimes(ticks);
+            string digital = clock.Time;
+            int days = clock.GetDays();
 
             Assert.True(digital == "23:11" && days == 1);
         }
@@ -276,137 +209,6 @@ namespace TrainEngine.Tests
             int ticks = 1448;
             int ticksToParse = ticks % 1440;
             Assert.True(ticksToParse == 8);
-        }
-        [Fact]
-        public void Simple_Clock_AssertCorrectString()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(8);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "0", "0", ":", "0", "8" });
-            Assert.True(equal);
-        }
-        [Fact]
-        public void Simple_Clock_Switch_TenSecondDigitToOne()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(10);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "0", "0", ":", "1", "0" });
-            Assert.True(equal);
-        }
-        [Fact]
-        public void Simple_Clock_Switch_HourDigitToOne()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(60);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "0", "1", ":", "0", "0" });
-            Assert.True(equal);
-        }
-        [Fact]
-        public void Simple_Clock_Switch_TenHourDigitToOne()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(600);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "1", "0", ":", "0", "0" });
-            Assert.True(equal);
-        }
-        [Fact]
-        public void Simple_Clock_Switch_TenHourDigitTo25()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(1500);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "2", "5", ":", "0", "0" });
-            Assert.False(equal);
-        }
-        [Fact]
-        public void Simple_Clock_1500Ticks_Assert()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(1500);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "0", "1", ":", "0", "0" });
-            Assert.True(equal);
-        }
-        [Fact]
-        public void Simple_Clock_Assert23()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(1380);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "2", "3", ":", "0", "0" });
-            Assert.True(equal);
-        }
-
-        [Fact]
-        public void Simple_Clock_AssertMidnight()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(1440);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "0", "0", ":", "0", "0" });
-            Assert.True(equal);
-        }
-        [Fact]
-        public void Simple_Clock_AssertMidnightTwoDays()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(2880);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "0", "0", ":", "0", "0" });
-            Assert.True(equal);
-        }
-        [Fact]
-        public void Simple_Clock_AssertMidnight01TwoDays()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(2881);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "0", "0", ":", "0", "1" });
-            Assert.True(equal);
-        }
-        [Fact]
-        public void Simple_Clock_AssertMidnight01()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(1441);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "0", "0", ":", "0", "1" });
-            Assert.True(equal);
-        }
-        [Fact]
-        public void Simple_Clock_AssertNextDay1111()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(2111);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "1", "1", ":", "1", "1" });
-            Assert.True(equal);
-        }
-        [Fact]
-        public void Simple_Clock_AssertNextDay2311()
-        {
-            var clock = new TwentyFourHourClock();
-            clock.Ticks = 0;
-            clock.TickTimes(2831);
-            var time = clock.Time;
-            var equal = Enumerable.SequenceEqual(time, new[] { "2", "3", ":", "1", "1" });
-            Assert.True(equal);
         }
     }
 }
