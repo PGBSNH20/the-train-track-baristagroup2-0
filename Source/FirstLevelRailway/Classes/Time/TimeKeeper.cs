99286478571bb57e309608497ff25dbc00f415fd
RobinAxelsson <axelsson.robin@outlook.com>
2021-03-15 12:23:20 +0100

    timekeeper edit

---
+++ Source/FirstLevelRailway/Classes/Time/TimeKeeper.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+
+namespace FirstLevelRailway
+{
+    public class TimeKeeper 
+    {
+        public delegate void Refresh();
+        public int Sleep { get; set; }
+        public IClock Clock { get; set; }
+        public TimeDisplayer Displayer { get; set; }
+        public TimeKeeper(IClock clock, TimeDisplayer displayer, int sleep)
+        {
+            //this.thread = thread;
+            Clock = clock;
+            Displayer = displayer;
+            Sleep = sleep;
+            Displayer.ClockToMemoryLayer(Clock);
+        }
+
+        public void StartTime(Refresh refreshMethod)
+        {
+            while (true)
+            {
+                Thread.Sleep(Sleep);
+                Clock.Tick();
+                Displayer.ClockToMemoryLayer(Clock);
+            }
+        }
+    }
+}
