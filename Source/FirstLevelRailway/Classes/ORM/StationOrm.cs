2e1df55df15c815dac5cea22f6529304cc21f5f4
RobinAxelsson <axelsson.robin@outlook.com>
2021-03-15 14:41:04 +0100

    RailwayParts moved

---
+++ Source/FirstLevelRailway/Classes/ORM/StationOrm.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FirstLevelRailway
+{
+    public class StationORM
+    { 
+        public List<Station> filePathStations(string stationsFilePath)
+        {
+            var stationList = new List<Station>();
+            string[] lines = File.ReadAllLines(stationsFilePath);
+
+            foreach (var line in lines)
+            {
+                string[] parts = line.Split('|');
+
+                var stationsInList = new Station
+                {
+                    Id = int.Parse(lines[0]),
+                    StationName = lines[1],
+                    EndStation = bool.Parse(lines[2])
+                };
+                stationList.Add(stationsInList);
+            }
+            return stationList;
+        }
+
+    }
+}
---
+++ Source/FirstLevelRailway/Classes/Railway/RailwayParts/Station.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLevelRailway
+{
+   public class Station : IRailwayPart
+    {
+        public int Id { get; set; }
+        public char Char { get; set; }
+        public int CoordinateX { get; set; }
+        public int CoordinateY { get; set; }
+        public string StationName { get; set; }
+        public bool EndStation { get; set; }
+        public List<IRailwayPart> Connections { get; set; } = new List<IRailwayPart>();
+    }
+}
