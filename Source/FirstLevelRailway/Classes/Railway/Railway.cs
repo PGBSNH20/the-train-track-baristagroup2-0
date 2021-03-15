62706cd1102de3086edd7101733e762fbd6a2daf
RobinAxelsson <axelsson.robin@outlook.com>
2021-03-15 14:52:54 +0100

    added railwayPartsORM

---
+++ Source/FirstLevelRailway/Classes/Railway/Railway.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLevelRailway
+{
+    public static class Railway
+    {
+        public const double PartLength_km = 10.0;
+        public static string Name { get; set; } = "Trans-Siberian Railway";
+        public static List<IRailwayPart> RailwayParts { get; set; } = new List<IRailwayPart>();
+        //public static List<TimeTable> timeTables { get; set; } = new List<TimeTable>();
+        public static List<IRailwayPart> GetRailwayParts()
+        {
+            return RailwayParts;
+        }
+        //public static int SetIdAddId(IRailwayItem newPart, int? id = null)
+        //{
+        //    var ids = RailwayItems.Select(x => x.Id).ToList();
+        //    if (id != null && ids.Contains((int)id))
+        //        throw new Exception($"Id already exists for {newPart.GetType()}");
+
+        //    if (RailwayItems.Count == 0 && id == null)
+        //    {
+        //        return newPart.Id = 1;
+        //    }
+        //    if (id == null)
+        //    {
+        //        int highest = RailwayItems.Select(x => x.Id).OrderBy(x => x).Last();
+        //        newPart.Id = highest + 1;
+        //        return highest + 1;
+        //    }
+        //    else
+        //    {
+        //        newPart.Id = (int)id;
+        //        return (int)id;
+        //    }
+        //}
+    }
+}
